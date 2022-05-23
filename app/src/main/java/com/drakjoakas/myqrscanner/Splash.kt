5bfc9762eafdd6e290c5b7399fd0e0e0cc3f3199
JOAQUIN SANDOVAL MIRAMONTES <joakas.11@gmail.com>
2022-05-22 22:16:45 -0500

    Nuevo Ícono. Se agregó splash antes de empezar.

---
+++ app/src/main/java/com/drakjoakas/myqrscanner/Splash.kt
@@ -0,0 +1,23 @@
+package com.drakjoakas.myqrscanner
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.drakjoakas.myqrscanner.databinding.ActivitySplashBinding
+import kotlin.concurrent.thread
+
+class Splash : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        thread {
+            Thread.sleep(3000)
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+    }
+}
